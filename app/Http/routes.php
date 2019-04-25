7af3948ba7fc47f139795a8bf2f2f88212ea616b
Manish Maharjan <manishnewar26@gmail.com>
2019-04-25 14:29:37 +0545

    Middleware part

---
+++ app/Http/routes.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::auth();
+
+
+Route::group(['prefix' => 'admin','middleware' => 'auth'],function(){
+    
+    Route::get('/home',[
+        'uses' => 'HomeController@index',
+        'as' => 'admin.home'
+    ]);
+
+    Route::get('/post/create',[
+        'uses' => 'PostsController@create',
+        'as' => 'post.create'
+    ]);
+    
+    Route::post('/post/store',[
+        'uses' => 'PostsController@store',
+        'as' => 'post.store'
+    ]);
+
+});
+
---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+use App\Category;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    public function category(){
+
+        return $this->belongsTo('App\Category');
+
+    }
+
+}
